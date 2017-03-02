0de6b018955e5262f23e1a99df1b1487a28869a0
yuqirong <yqr271228943@gmail.com>
2017-03-02 22:28:21 +0800

    添加探探效果的demo

--- app/src/main/java/com/yuqirong/cardswipeview/MainActivity.java
+++ app/src/main/java/com/yuqirong/cardswipeview/MainActivity.java
@@ -5,11 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,62 +23,42 @@ import me.yuqirong.cardswipeview.OnSwipeListener;
 public class MainActivity extends AppCompatActivity {
 
     private List<String> list = new ArrayList<>();
-    private static final String TAG = "Main";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        list.add("D");
-        list.add("E");
-        list.add("F");
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initView() {
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        Button button = (Button) findViewById(R.id.btn);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(new RecyclerView.Adapter() {
-            @Override
-            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                Log.i(TAG, "onCreateViewHolder");
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
-                return new MyViewHolder(view);
-            }
-
-            @Override
-            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-                Log.i(TAG, "position = " + position);
-                ((MyViewHolder) holder).textView.setText(list.get(position));
-            }
-
-            @Override
-            public int getItemCount() {
-                return list.size();
-            }
-
-            class MyViewHolder extends RecyclerView.ViewHolder {
-
-                TextView textView;
-
-                MyViewHolder(View itemView) {
-                    super(itemView);
-                    textView = (TextView) itemView.findViewById(R.id.tv);
-                }
-
-            }
-        });
+        recyclerView.setAdapter(new MyAdapter());
         CardItemTouchHelperCallback cardCallback = new CardItemTouchHelperCallback(recyclerView.getAdapter(), list);
         cardCallback.setOnSwipedListener(new OnSwipeListener<String>() {
 
             @Override
             public void onSwiping(RecyclerView.ViewHolder viewHolder, float ratio, int direction) {
-                viewHolder.itemView.setAlpha(1 - Math.abs(ratio) + 0.2f);
+                MyAdapter.MyViewHolder myHolder = (MyAdapter.MyViewHolder) viewHolder;
+                viewHolder.itemView.setAlpha(1 - Math.abs(ratio) * 0.2f);
+                if(direction == CardConfig.SWIPING_LEFT){
+                    myHolder.dislikeImageView.setAlpha(Math.abs(ratio));
+                }else if(direction == CardConfig.SWIPING_RIGHT){
+                    myHolder.likeImageView.setAlpha(Math.abs(ratio));
+                }else{
+                    myHolder.dislikeImageView.setAlpha(0f);
+                    myHolder.likeImageView.setAlpha(0f);
+                }
             }
 
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, String o, int direction) {
+                MyAdapter.MyViewHolder myHolder = (MyAdapter.MyViewHolder) viewHolder;
                 viewHolder.itemView.setAlpha(1f);
+                myHolder.dislikeImageView.setAlpha(0f);
+                myHolder.likeImageView.setAlpha(0f);
                 Toast.makeText(MainActivity.this, direction == CardConfig.SWIPED_LEFT ? "left" : "right", Toast.LENGTH_SHORT).show();
             }
 
@@ -89,12 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 recyclerView.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        list.add("A");
-                        list.add("B");
-                        list.add("C");
-                        list.add("D");
-                        list.add("E");
-                        list.add("F");
+                        initData();
                         recyclerView.getAdapter().notifyDataSetChanged();
                     }
                 }, 3000L);
@@ -105,6 +79,49 @@ public class MainActivity extends AppCompatActivity {
         final CardLayoutManager cardLayoutManager = new CardLayoutManager(recyclerView, touchHelper);
         recyclerView.setLayoutManager(cardLayoutManager);
         touchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    private void initData() {
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        list.add("F");
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+//            TextView textView = ((MyViewHolder) holder).textView;
+//            textView.append(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView textView;
+            ImageView likeImageView;
+            ImageView dislikeImageView;
+
+            MyViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView) itemView.findViewById(R.id.tv_name);
+                likeImageView = (ImageView) itemView.findViewById(R.id.iv_like);
+                dislikeImageView = (ImageView) itemView.findViewById(R.id.iv_dislike);
+            }
+
+        }
     }
 
 }
